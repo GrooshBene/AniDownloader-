0bf7cd7af081cc3c626e058f9483eb6138fb2bda
GrooshBene <grooshbene@GrooshBeneui-MacBook-Air.local>
2016-01-08 17:46:49 +0900

    Monongo Connected

--- database.py
+++ database.py
@@ -1,30 +1,9 @@
-#!/usr/bin/python
+#2016. 1. 8 Made By GrooshBene
 
-import MySQLdb
+import pymongo
+from pymongo import MongoClient
+client = MongoClient()
 
-fileUrl = "/Volumes/Seagate Backup Plus Drive/Projects/AniDownloader-/test"
-
-fileFormat = ".txt"
-val = input("Plz Input the Value : ")
-
-db = MySQLdb.connect("localhost", "root", "wltn6705", "pythontest")
-
-cursor = db.cursor()
-
-cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
-
-sql = """CREATE TABLE FILES (
-         id INT,
-         subtitle   CHAR(100),
-         date   INT
-)"""
-
-cursor.execute(sql)
-for i in range(0,val):
-    record = "INSERT INTO FILES(id,subtitle, date) VALUES ('%d', '%s', '%d')" % (i, fileUrl + (i+1) + fileFormat, new Date());
-    try:
-        cursor.execute(record)
-        db.commit()
-    except:
-        db.rollback()
-db.close()
+db = client.ani24
+ani_col = db.adi
+video_col = db.video
